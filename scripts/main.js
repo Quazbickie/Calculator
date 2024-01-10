a72b68c5ae4aac03b15f2735402672663ec2dd92
Ryan <ryankane180@gmail.com>
2024-01-10 18:44:15 +0000

    Js

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let buttons = document.querySelector('.buttons');
+let displayBuffer = [];
+
+
+buttons.addEventListener('click', function(e) {
+    let target = e.target;
+
+    console.log(target);
+
+    
+
+
+})
